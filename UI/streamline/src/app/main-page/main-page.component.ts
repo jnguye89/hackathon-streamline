4f4ae7aac4d4e5e0a1b9f5c94b1a904addf7eb41
Jackie Nguyen <jnguye89@gmail.com>
2025-04-26 13:41:03 -0700

    added styling

--- UI/streamline/src/app/main-page/main-page.component.ts
+++ UI/streamline/src/app/main-page/main-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule,MatDividerModule,FlexLayoutModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
