0341a4d4e4af74c4866a37e7690a6966ef96042d
Jackie Nguyen <jnguye89@gmail.com>
2025-04-26 14:12:10 -0700

    added autoplay video

--- UI/streamline/src/app/main-page/main-page.component.ts
+++ UI/streamline/src/app/main-page/main-page.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatChipsModule } from '@angular/material/chips';
+import { VideoService } from '../services/video-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule,MatDividerModule,FlexLayoutModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatChipsModule,
+    HttpClientModule
+  ],
+  providers: [VideoService],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
+  constructor(private videoService: VideoService) {}
 
+  ngOnInit() {
+    this.videoService.getVideos().subscribe((videos) => {
+      console.log(videos);
+    });
+  }
 }
