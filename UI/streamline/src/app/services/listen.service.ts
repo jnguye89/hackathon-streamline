12229cca41196edc63f7018405f7f4f1973abc98
Jackie Nguyen <jnguye89@gmail.com>
2025-04-30 21:38:57 -0700

    pipieline changes

--- UI/streamline/src/app/services/listen.service.ts
+++ UI/streamline/src/app/services/listen.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ListenService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
--- video-player-backend/server.js
+++ video-player-backend/server.js
@@ -13,13 +13,13 @@ const { OAuth2 } = google.auth;
 const app = express();
 const streamifier = require("streamifier");
 const admin = require("firebase-admin");
-const serviceAccount = require("./streamline-39164-firebase-adminsdk-fbsvc-9f85dddc6e.json");
+const serviceAccount = require("./serviceAccountKey.json");
 const { file } = require("googleapis/build/src/apis/file");
 const radioService = require("./radio.service.js");
 
 require("dotenv").config();
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount) ?? admin.credential.applicationDefault(),
   storageBucket: "streamline-39164.firebasestorage.app",
 });
 
